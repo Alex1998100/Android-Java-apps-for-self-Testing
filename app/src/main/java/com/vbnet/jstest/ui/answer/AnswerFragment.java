b69bc4319d85d37538441695e86e93d784d60367
Admin <admin@opensuse.com>
2024-08-15 14:16:03 +0300

    first commit

---
+++ app/src/main/java/com/vbnet/jstest/ui/answer/AnswerFragment.java
@@ -0,0 +1,66 @@
+package com.vbnet.jstest.ui.answer;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.vbnet.jstest.MainActivity;
+import com.vbnet.jstest.databinding.FragmentAnswerBinding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class AnswerFragment extends Fragment {
+
+    private FragmentAnswerBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        //AnswerViewModel answerViewModel = new ViewModelProvider(this).get(AnswerViewModel.class);
+
+        binding = FragmentAnswerBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        String answerFile = ((MainActivity)this.getContext()).currentQuestion+".txt";
+
+        final TextView textView = binding.textAnswer;
+        textView.setText(readTextFile(answerFile));
+        //answerViewModel".getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    public  String readTextFile(String fileName) {
+        BufferedReader reader;
+        StringBuilder sb = new StringBuilder();
+       try{
+            final InputStream file = this.getContext().getAssets().open(fileName);
+            reader = new BufferedReader(new InputStreamReader(file));
+            String line = reader.readLine();
+            while(line != null){
+                line = reader.readLine();
+                sb.append(line);
+                sb.append("\n");
+            }
+
+        } catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+        return  sb.toString();
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/com/vbnet/jstest/ui/notes/NotesFragment.java
@@ -0,0 +1,137 @@
+package com.vbnet.jstest.ui.notes;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.vbnet.jstest.MainActivity;
+import com.vbnet.jstest.databinding.FragmentNotesBinding;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executor;
+
+public class NotesFragment extends Fragment {
+
+    private FragmentNotesBinding binding;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "AnonymousAuth";
+    String answerFile;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        //NotesViewModel notesViewModel =  new ViewModelProvider(this).get(NotesViewModel.class);
+
+        binding = FragmentNotesBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        answerFile = ((MainActivity) this.getContext()).currentQuestion;
+
+        final TextView textView = binding.title;
+        textView.setText(answerFile);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("notes");
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        binding.submitNotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitPost();
+            }
+        });
+
+        return root;
+    }
+
+    private void submitPost() {
+        String REQUIRED = "Required";
+        final String title = binding.title.getText().toString();
+        final String body = binding.notes.getText().toString();
+        // Body is required
+        if (TextUtils.isEmpty(body)) {
+            binding.title.setError(REQUIRED);
+            return;
+        }
+        // Disable button so there are no multi-posts
+        setEditingEnabled(false);
+        Toast.makeText(getContext(), "Posting...", Toast.LENGTH_SHORT).show();
+
+        mAuth.signInAnonymously().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInAnonymously:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInAnonymously:failure", task.getException());
+                }
+            }
+        });
+
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.exists()) {
+                    String key = mDatabase.push().getKey();
+                    Map<String, Object> newNotes = new HashMap<>();
+                    newNotes.put( key, new Object() {
+                        public final String Q = title;
+                        public final String T = body;
+                    });
+                    Task<Void> voidTask = mDatabase.updateChildren(newNotes);
+                    setEditingEnabled(true);
+                }
+            }
+
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w(TAG, "getUser:onCancelled", databaseError.toException());
+                setEditingEnabled(true);
+            };
+        });
+    }
+
+    ;
+
+
+    private void setEditingEnabled(boolean enabled) {
+        binding.title.setEnabled(enabled);
+        binding.notes.setEnabled(enabled);
+        if (enabled) {
+            binding.submitNotes.show();
+        } else {
+            binding.submitNotes.hide();
+        }
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
